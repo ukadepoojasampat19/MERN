842633bd2792c69f4826b840efd86444379a79bd
ukadepoojasampat19 <poojaukade8@gmail.com>
2023-12-12 00:13:56 +0530

    create sign up page

--- my-client-app/vite.config.js
+++ my-client-app/vite.config.js
@@ -3,5 +3,14 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    proxy: {
+      "/API": {
+        target: "http://localhost:3000",
+        secure: false,
+      },
+    },
+  },
   plugins: [react()],
 })
+// https://vitejs.dev/config/
